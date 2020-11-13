5424e505a49784fa076ae41ca5b9d4a10c643d42
Skid Vis <skid@skidvis.com>
2020-11-13 08:08:41 -0600

    033 - QuestDoors

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Mover : MonoBehaviour
+{
+    private InputDevice _device;
+    private CharacterController _character;
+    private Vector2 _inputAxis;
+
+    private void Start()
+    {
+        _device = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+        _character = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        _device.TryGetFeatureValue(CommonUsages.primary2DAxis, out _inputAxis);
+        _character.Move(new Vector3(_inputAxis.x, 0, _inputAxis.y) * Time.deltaTime * 1f);
+    }
+}
---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField] private Transform theDoor;
+    private float startX;
+    private bool isOpen;
+
+    private void Start()
+    {
+        startX = theDoor.position.x;
+    }
+
+    private void Update()
+    {
+        if (isOpen && theDoor.position.x < startX + 1f)
+        {
+            theDoor.position += theDoor.right * Time.deltaTime;
+        }
+        
+        if (isOpen == false && theDoor.position.x > startX)
+        {
+            theDoor.position -= theDoor.right * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "key")
+        {
+            isOpen = true;   
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "key")
+        {
+            isOpen = false;   
+        }
+    }
+}
